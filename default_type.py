32ddce259ca1a26f5bf4e9d0c99dcb9f25173c4b
Hwanryan <cagameku3842@naver.com>
2023-06-06 23:41:58 +0900

    파이썬 기본 자료형

---
+++ default_type.py
@@ -0,0 +1,22 @@
+'''
+기본 자료형
+'''
+#int : 정수
+#float : 실수
+#bool : 논리
+#str : 문자열
+
+#문자열 인덱싱
+str = "HELLO"
+str[1] # >> "H"
+str[-1] # >> "O"
+
+#문자열 슬라이싱
+#s[start:stop:step]
+#start : 시작 인덱스
+#stop : 종료 인덱스
+#step : 증감값
+str[0:3] # >> "HEL"
+str[0:4:2] # >> "HL"
+
+
